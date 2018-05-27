1b0f1eab703a2a298aa19a8a3251fa5a73dbe7af
mgregory-carecloud <mgregory@carecloud.com>
2018-05-27 03:14:14 -0400

    Added annotate gem for documentation

--- app/models/token.rb
+++ app/models/token.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tokens
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  value      :string
+#  expiration :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Token < ApplicationRecord
   belongs_to :user
   validates :value, presence: true
--- spec/models/token_spec.rb
+++ spec/models/token_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tokens
+#
+#  id         :bigint(8)        not null, primary key
+#  user_id    :bigint(8)
+#  value      :string
+#  expiration :date
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Token, type: :model do
